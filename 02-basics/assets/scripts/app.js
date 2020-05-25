ed5dbf42aa4202919adaf1667e69cd3fe21d9e1b
Aidan <aidansoftwaredev@gmail.com>
2020-05-25 21:17:57 +0100

    Basics 2

---
+++ 02-basics/assets/scripts/app.js
@@ -0,0 +1,7 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+
+currentResult = currentResult + 10;
+let calculationDescription = `${defaultResult} + ${currentResult}`;
+
+outputResult(currentResult, calculationDescription);
