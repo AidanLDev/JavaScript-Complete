55c600771d7c8610e8df09f5d4cebbd93ef39226
Aidan <aidansoftwaredev@gmail.com>
2020-06-03 07:30:29 +0100

    Finsihing chap2

--- 02-basics/assets/scripts/app.js
+++ 02-basics/assets/scripts/app.js
@@ -1,7 +1,6 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
-
-currentResult = currentResult + 10;
+let logEntries = [];
 
 function getUserInput() {
   return parseInt(userInput.value);
@@ -11,44 +10,59 @@ function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
   return `${resultBeforeCalc} ${operator} ${calcNumber}`;
 }
 
+function writeToLog(operation, prevResult, enteredNumber, currentResult) {
+  const logEntry = {
+    operation: operation,
+    prevNum: prevResult,
+    enteredNumber: enteredNumber,
+    currentResult: currentResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
 function add() {
   const enteredNumber = getUserInput();
   const resultBeforeCalc = currentResult;
-  currentResult = currentResult + enteredNumber;
+  currentResult += enteredNumber;
   outputResult(
     currentResult,
     createAndWriteOutput('+', resultBeforeCalc, enteredNumber)
   );
+  writeToLog('+', resultBeforeCalc, enteredNumber, currentResult);
 }
 
 function subtract() {
   const enteredNumber = getUserInput();
   const resultBeforeCalc = currentResult;
-  currentResult = currentResult - enteredNumber;
+  currentResult -= enteredNumber;
   outputResult(
     currentResult,
     createAndWriteOutput('-', resultBeforeCalc, enteredNumber)
   );
+  writeToLog('-', resultBeforeCalc, enteredNumber, currentResult);
 }
 
 function multiply() {
   const enteredNumber = getUserInput();
   const resultBeforeCalc = currentResult;
-  currentResult = currentResult * enteredNumber;
+  currentResult *= enteredNumber;
   outputResult(
     currentResult,
     createAndWriteOutput('*', resultBeforeCalc, enteredNumber)
   );
+  writeToLog('*', resultBeforeCalc, enteredNumber, currentResult);
 }
 
 function divide() {
   const enteredNumber = getUserInput();
   const resultBeforeCalc = currentResult;
-  currentResult = currentResult / enteredNumber;
+  currentResult /= enteredNumber;
   outputResult(
     currentResult,
     createAndWriteOutput('/', resultBeforeCalc, enteredNumber)
   );
+  writeToLog('/', resultBeforeCalc, enteredNumber, currentResult);
 }
 
 addBtn.addEventListener('click', add);
